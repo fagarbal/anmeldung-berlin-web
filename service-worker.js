9897795586e0cdb7eda9b64d9b67868acb0b3d98
fagarbal <fagarbal@gmail.com>
2019-05-09 13:44:48 +0200

    update web

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67fb48afbc906191ac9d4c07db39d337.js"
+  "/precache-manifest.736f715504d0ae4c3bea903b487e2ab7.js"
 );
 
 workbox.clientsClaim();
