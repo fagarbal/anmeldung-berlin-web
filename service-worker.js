27991f7d1f0e320e327f026f6f270ef605357c37
Fabio Garcia Balbuena <fagarbal@gmail.com>
2019-05-31 13:53:45 +0200

    update web

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a297dccb5202099e5dbc53041b0e5b3b.js"
+  "/precache-manifest.a7dded1bf422f39bcfc4e99dc87a4f39.js"
 );
 
 self.addEventListener('message', (event) => {
